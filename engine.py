9b7de3899508b42da5c02c9ce7b1faddde62c3b6
Zemke <florian@zemke.io>
2021-11-10 20:40:31 +0100

    More on all ends

--- engine.py
+++ engine.py
@@ -1,7 +1,6 @@
 import requests
 
 class Engine:
-  COLOR = 'BLACK'
 
   def __init__(self, board_id, url=None):
     self.board_id = board_id
@@ -19,38 +18,37 @@ class Engine:
     print('  at', f'{self.url}/board/{self.board_id}/turn')
     return requests.post(url = f'{self.url}/board/{self.board_id}/turn', json=pl)
 
+  def termianl(self):
+    pass  # TODO
 
 """
-Board statte
+"captured" : [],
+"castlingAllowed" : [],
+"grid" : [ [
+      {
+         "color" : "BLACK",
+         "id" : "8c8ee347-f88e-45a0-be57-c9322154bab2",
+         "name" : "Rook",
+         "position" : {
+            "file" : 0,
+            "rank" : 0
+         },
+         "side" : "QUEENSIDE"
+.
+.
+.
+"id" : "d87ba920-8fcc-4539-9186-5e142c1ce5bf",
+"movements" : [],
+"nextTurn" : "WHITE",
+"uuidBlack" : null,
+"uuidWhite" : "fcd1c27e-8e11-412f-8de8-4ee54b8b9ea7"
+def __init__(self, grid, next_turn):
+  self.grid = grid
+  next_turn = next_turn
 
-class State:
-  "captured" : [],
-  "castlingAllowed" : [],
-  "grid" : [ [
-        {
-           "color" : "BLACK",
-           "id" : "8c8ee347-f88e-45a0-be57-c9322154bab2",
-           "name" : "Rook",
-           "position" : {
-              "file" : 0,
-              "rank" : 0
-           },
-           "side" : "QUEENSIDE"
-  .
-  .
-  .
-  "id" : "d87ba920-8fcc-4539-9186-5e142c1ce5bf",
-  "movements" : [],
-  "nextTurn" : "WHITE",
-  "uuidBlack" : null,
-  "uuidWhite" : "fcd1c27e-8e11-412f-8de8-4ee54b8b9ea7"
-  def __init__(self, grid, next_turn):
-    self.grid = grid
-    next_turn = next_turn
-
-  def from_engine(self, json):
-    return State(
-      grid = json.
-    )
+def from_engine(self, json):
+  return State(
+    grid = json.
+  )
 """
 
--- winning.py
+++ winning.py
@@ -1,39 +1,104 @@
 from engine import Engine
+from math import inf
+
+# TODO En passant and castle
 
 board_id = input()
-
+print("Board ID:", eng.board_id)
 eng = Engine(board_id)
 
-print("Board ID", eng.board_id)
-
-
 class Maximizer:
+  C = 'BLACK'
+
   def __init__(self, eng):
     self.eng = eng
 
   def move(self):
     board = eng.board()
     print(board)
+
+    # TODO for now just taking the first turn, but be clever
     piece = None
     for d1 in board["grid"]:
       for p in d1:
         if p is None:
           continue
-        if p['color'] == Engine.COLOR and p['name'] == 'Pawn':
+        if p['color'] == self.C and p['name'] == 'Pawn':
           piece = p
     print('piece', piece)
     nxx = eng.next_turns(piece['id'])
     print(nxx)  #[{'file': 7, 'rank': 2}, {'file': 7, 'rank': 3}]
     eng.turn(piece['id'], nxx[0])
-    #eng.turn(nxx[0])
-    #self.minimax()
-    pass
 
-  def minimax(self):
-    pass
+    # here's smartness
+    minimax(1, State.from_engine(board), True)
+
+  def heuristic(self, state):
+    pieces_v = { 'q': 5, 'r': 4, 'b': 3, 'k': 2, 'p': 1 }
+    maxim = state.pieces(True)
+    minim = state.pieces(False)
+    v_maxim = 0
+    for p in maxim:
+      v_maxim += self.pieces_v[p]
+    v_minim = 0
+    for p in minim:
+      v_maxim -= self.pieces_v[p.lower()]
+    return v_maxim
+
+  def minimax(self, look_ahead, state, maxim):
+    terminal = eng.terminal(state)
+    if terminal:
+      return -inf if maxim else +inf
+    elif look_ahead == 0:
+      # TODO a heuristic may also be a neural network
+      return self.heuristic(state)
+    # TODO for testing moves engine requires state changes
+    #  without mutating the original board
+    if maxim:
+      v = -inf
+      for t in self.eng.next_turns():
+        turn = t  # TODO move after turn
+        v = max(v, minimax(t, look_ahead-1, False))
+    else:
+      v = +inf
+      for t in self.eng.next_turns():
+        turn = t  # TODO move after turn
+        v = max(v, minimax(t, look_ahead-1, True))
+    return v
+
+class State:
+  def __init__(self, grid, next_turn):
+    self.grid = grid
+    self.next_turn = next_turn
+
+  def pieces(self, black):
+    pp = []
+    for rank in self.grid:
+      for p in rank:
+        if p is None:
+          continue
+        elif black and p.islower()
+          pp.append(p)
+        elif not black and p.isupper():
+          pp.append(p)
+    return pp
+
+  @staticmethod
+  def from_engine(self, json):
+    g = json['grid']
+    r = [[[] for i in range(9)]]
+    for row in g:
+      for p in g:
+        if p is None:
+          n = None
+        else:
+          n = 'x' if p['name'] == 'King' else p['name'][0]
+          n = n.lower() if p['color'] == Maxmimizer.C else n.upper()
+        r[p['position']['rank']][p['position']['file']] = n
+    return State(grid=r, next_turn=json['nextTurn'])
+
 
 maxim = Maximizer(eng)
-
 
 while 1:
   print('enter to move')
