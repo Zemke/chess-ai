52ae86c141305b3056018d4619d6dfc881b1fadf
Zemke <florian@zemke.io>
2021-11-14 22:54:29 +0100

    Refactoring with new state driver

--- engine.py
+++ engine.py
@@ -1,32 +1,113 @@
 import requests
+import chess
+
+
+class State:
+  pieces_v = { 'q': 4, 'r': 2, 'b': 2, 'n': 2, 'p': 1, 'k': 0 }
+
+  def __init__(self, board):
+    self.board = board
+    self.v = self.heuristic()
+    self.key = ' '.join(self.board.fen().split(' ')[:2])
+
+  def pieces(self, black):
+    res = []
+    c = chess.BLACK if black else chess.WHITE
+    for p in self.board.piece_map().values():
+      if p.color == c:
+        res.append(p.symbol())
+    return res
+
+  def heuristic(self):
+    maxim = self.pieces(True)
+    minim = self.pieces(False)
+    v = 0
+    lmf = .01 * self.board.legal_moves.count()
+    for p in maxim:
+      v += self.pieces_v[p]
+      v += lmf
+    for p in minim:
+      v -= self.pieces_v[p.lower()]
+      v -= lmf
+    return v
+
+  @staticmethod
+  def from_uci(uci):
+    return chess.Move.from_uci(uci)
+
+  @staticmethod
+  def square_rank(sq):
+    return chess.square_rank(sq)
+
+  @staticmethod
+  def square_file(sq):
+    return chess.square_file(sq)
+
+  @staticmethod
+  def from_engine(json):
+    g = json['grid']
+    fen = ''
+    for ri in range(8):
+      empc = 0
+      for ci in range(8):
+        p = g[ri][ci]
+        if p is None:
+          empc += 1
+        else:
+          if empc != 0:
+            fen += str(empc)
+            empc = 0
+          n = 'n' if p['name'] == 'Knight' else p['name'][0]
+          n = n.lower() if p['color'] == 'BLACK' else n.upper()
+          fen += n
+      if empc != 0:
+        fen += str(empc)
+        empc = 0
+      if ri != 7:
+        fen += '/'
+    fen += ' '
+    fen += 'b' if json['nextTurn'] == 'BLACK' else 'w'
+    fen += ' '
+    cstl = ''.join(list(map(lambda s: s[0], json['castlingAllowed'])))
+    if len(cstl) > 0:
+      fen += cstl.upper() if json['nextTurn'] == 'BLACK' else cstl.lower()
+    else:
+      fen += '-'
+    fen += ' - 0 0'
+    return State(chess.Board(fen))
+
 
 class Engine:
 
-  def __init__(self, url=None):
-    self.url = url if url is not None else "http://localhost:9999/api"
+  def __init__(self, board_id):
+    self.url = "http://localhost:9999/api"
+    self.board_id = board_id
+    self.refresh()
 
-  def board(self, board_id):
-    return self.__res(requests.get(url = f'{self.url}/board/{board_id}'))
+  def refresh(self):
+    self.board = \
+      self.__res(requests.get(url = f'{self.url}/board/{self.board_id}'))
+    if self.board_id != self.board['id']:
+      raise Exception(f"{self.board_id} != {self.board['id']}")
 
-  def next_turns(self, board_id, piece_id):
-    return self.__res(requests.get(url = f'{self.url}/board/{board_id}/piece/{piece_id}/allowed-next-positions'))
-
-  def turn(self, board_id, piece, target, spinoff=False):
-    pl = {"piece": piece, "target": target}
+  def turn(self, m):
+    target = {
+      "rank": 7-State.square_rank(m.to_square),
+      "file": State.square_file(m.to_square)
+    }
+    f = (7-State.square_rank(m.from_square), State.square_file(m.from_square))
+    piece = self.board['grid'][f[0]][f[1]]
+    #print(f'from {f} to {target} via {m} piece {piece}')
+    pl = {"piece": piece['id'], "target": target}
     if target['rank'] == 0 or target['rank'] == 7:
       pl['promotion'] = 'queen'
-    params = {'spinoff': "1" if spinoff else "0"}
-    return self.__res(requests.post(url=f'{self.url}/board/{board_id}/turn',
-                                    json=pl,
-                                    params=params))
+    return self.__res(
+      requests.post(url=f'{self.url}/board/{self.board_id}/turn', json=pl))
 
-  def turns(self, board_id):
-    return self.__res(requests.get(url=f'{self.url}/board/{board_id}/turns'))
-
-  def castle(self, board_id, side, spinoff=False):
+  def castle(self, side, spinoff=False):
     if side != 'KINGSIDE' and side != 'QUEENSIDE':
       raise Exception("Invalid side " + str(side))
-    return self.__res(requests.post(url=f'{self.url}/board/{board_id}/castle/{side}'))
+    return self.__res(requests.post(url=f'{self.url}/board/{self.board_id}/castle/{side}'))
 
   def __res(self, res):
     try:
--- winning.py
+++ winning.py
@@ -1,171 +1,79 @@
-from engine import Engine
+from engine import Engine, State
 from math import inf
 from time import time
 
 
 class Maximizer:
   C = 'BLACK'
-  __DP = {}
+  S = {}
 
-  def __init__(self, eng, board_id):
-    self.eng = eng
-    self.board_id = board_id
+  def __init__(self, engine):
+    self.engine = engine
 
   def move(self):
-    board = State.from_engine(eng.board(self.board_id))
-    tt = self.minimax(3, board, True, -inf, +inf)
-    tt.sort(key=lambda x: x[0], reverse=True)
-    v, m = tt[0]
-    if m.castling:
-      self.eng.castle(self.board_id, m.castling)
-    else:
-      self.eng.turn(self.board_id, m.piece, m.target)
-    return v
+    self.engine.refresh()
+    s = State.from_engine(self.engine.board)
+    S_before = len(self.S)
+    mm = self.minimax(5, s, True, -inf, +inf)
+    mm.sort(key=lambda x: x[0], reverse=True)
+    v, m = mm[0]
+    self.engine.turn(m)
+    return v, len(self.S)-S_before
 
-  def heuristic(self, state):
-    pieces_v = { 'q': 4, 'r': 2, 'b': 2, 'n': 2, 'p': 1, 'k': 0 }
-    maxim = state.pieces(True)
-    minim = state.pieces(False)
-    v = 0
-    for p in maxim:
-      v += pieces_v[p]
-    for p in minim:
-      v -= pieces_v[p.lower()]
-    return v
-
-  def minimax(self, la, state, maxim, a, b, rett=True):
-    if rett:
-      tt = []
-    if state.checkmated:
+  def minimax(self, la, s, maxim, a, b, ret=True):
+    if ret:
+      vm = []
+    if s.board.is_checkmate():
       return -inf if maxim else +inf
+    elif s.board.is_stalemate():
+      return 0
     elif la == 0:
-      # TODO a heuristic may also be a neural network
-      return self.heuristic(state)
-    spb = self.__spinoff_boards(state, maxim)
-    if la <= 1:
-      spb = spb[:10]
+      # a heuristic may also be a neural network
+      return s.v
+
+    opts = []
+    for m in s.board.legal_moves:
+      cp = s.board.copy(stack=False)
+      cp.push(m)
+      ns = State(cp)
+      opts.append((m, ns))
+    opts.sort(key=lambda o: o[1].v, reverse=maxim)
+
+    # beam
+    if la <= 2:
+      opts = opts[:10]
+
     v = -inf if maxim else +inf
-    for t, s in spb:
-      if s in self.__DP:
-        mx = self.__DP[s]
-      else:
-        mx = self.minimax(la-1, s, not maxim, a, b, False)
-        self.__DP[s] = mx
-      if rett:
-        tt.append((mx, t))
+    for m, s in opts:
+      if s.key not in self.S:
+        # TODO prune S
+        self.S[s.key] = self.minimax(la-1, s, not maxim, a, b, False)
+      if ret:
+        vm.append((self.S[s.key], m))
       if maxim:
-        v = max(v, mx)
+        v = max(v, self.S[s.key])
         a = max(a, v)
       else:
-        v = min(v, mx)
+        v = min(v, self.S[s.key])
         b = min(b, v)
       if b <= a:
-        break
-    return tt if rett else v
+        break # alpha-beta cutoff
 
-  def __spinoff_boards(self, state, maxim):
-    res = []
-    for side in state.castling:
-      s = State.from_engine(self.eng.castle(state.board_id, side, True))
-      res.append((self.heuristic(s), Move.castling(side), s))
-    for turn in self.eng.turns(state.board_id):
-      for target in turn['targets']:
-        piece = turn['piece']['id']
-        s = State.from_engine(
-          self.eng.turn(state.board_id, piece, target, True))
-        res.append((self.heuristic(s), Move.turn(piece, target), s))
-    res.sort(key=lambda x: x[0], reverse=maxim)
-    return [[r[1], r[2]] for r in res]
+    return vm if ret else v
 
 
-class Move:
-  def __init__(self, piece=None, target=None, castling=False):
-    self.piece = piece
-    self.target = target
-    self.castling = castling  # QUEENSIDE, KINGSIDE
+# TODO en passant, castling, promotion
 
-  @staticmethod
-  def castling(side):
-    return Move(castling=side)
-
-  @staticmethod
-  def turn(piece, target):
-    return Move(piece=piece, target=target)
-
-  def __repr__(self):
-    if self.castling:
-      return str(self.castling)
-    return str((self.piece, self.target))
-
-
-class State:
-  def __init__(self, board_id, grid, next_turn, checkmated, castling):
-    self.board_id = board_id
-    self.grid = grid
-    self.next_turn = next_turn
-    self.checkmated = checkmated
-    self.castling = castling
-
-  def pieces(self, black):
-    pp = []
-    for rank in self.grid:
-      for p in rank:
-        if p is None:
-          continue
-        elif black and p.islower():
-          pp.append(p)
-        elif not black and p.isupper():
-          pp.append(p)
-    return pp
-
-  @staticmethod
-  def from_engine(json):
-    g = json['grid']
-    r = [[None for k in range(8)] for i in range(8)]
-    for ri in range(8):
-      for ci in range(8):
-        p = g[ri][ci]
-        if p is not None:
-          n = 'n' if p['name'] == 'Knight' else p['name'][0]
-          n = n.lower() if p['color'] == Maximizer.C else n.upper()
-          r[ri][ci] = n
-    return State(board_id=json['id'],
-                 grid=r,
-                 next_turn=json['nextTurn'],
-                 checkmated=json['checkmated'],
-                 castling=json['castlingAllowed'])
-
-  def __repr__(self):
-    s = ''
-    for row in self.grid:
-      s += ' '.join([i if i is not None else ' ' for i in row])
-      s += '\n'
-    return s
-
-  def __eq__(self, o):
-    if not isinstance(o, State):
-      return False
-    return (self.grid == o.grid
-        and self.next_turn == o.next_turn
-        and self.checkmated == o.checkmated
-        and self.castling == o.castling)
-
-  def __hash__(self):
-    return hash(
-      (tuple([tuple(i) for i in self.grid]),
-      self.next_turn,
-      self.checkmated,
-      tuple(self.castling)))
-
-
-eng = Engine()
-maxim = Maximizer(eng, input())
+print("paste game uuid")
+maxim = Maximizer(Engine(input()))
 
 while 1:
   print('enter to move')
+  tw = time()
   nex = input()
+  print(f'waited for {time()-tw:.2f}s')
   print('thinking...')
   t = time()
-  v = maxim.move()
-  print(f'took me {time()-t:.2f}s for v={v}')
+  v, vis = maxim.move()
+  print(f'took me {time()-t:.2f}s v{v} {vis} new opts')
 
